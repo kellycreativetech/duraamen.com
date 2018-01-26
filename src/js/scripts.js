d8b72fca3ffa2b78ea4b7b78360f454fbd22a71e
Kasey Kelly <kasey@servee.com>
2018-01-26 07:28:17 -0800

    video galleries, fancybox added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,14 +6,6 @@ $('.button-collapse').sideNav({
   edge: 'right' // Choose the horizontal origin
 });
 
-// var el = document.querySelector('.custom-scrollbar');
-// Ps.initialize(el);
-
-// Tooltips Initialization
-// $(function () {
-//   $('[data-toggle="tooltip"]').tooltip()
-// })
-
 $('.toggle-title').on('click', function(){
   $(this).next('.toggle-this').toggleClass('open');
   $(this).toggleClass('open');
